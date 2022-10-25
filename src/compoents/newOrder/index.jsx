3ed8ce2f4cb64297df26a7aa6c64cc024d70a99a
Movsar <gbeshtoev1@gmail.com>
2022-10-25 23:51:59 +0300

    fix2

--- src/compoents/newOrder/index.jsx
+++ src/compoents/newOrder/index.jsx
@@ -1,91 +1,112 @@
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { removeOrder, setOrder } from '../../Slice/orderSlice';
-import { Link } from 'react-router-dom';
-import Style from './newOrder.module.scss';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { removeOrder, setOrder } from "../../Slice/orderSlice";
+import { Link } from "react-router-dom";
+import Style from "./newOrder.module.scss";
 
 function NewOrder() {
   const dispatch = useDispatch();
   const orders = useSelector((state) => state.orderSlice.order);
   const inputRef = React.useRef();
 
-  const [number, setNumber] = useState('');
+  const [getNumber, setGetNumber] = useState("");
 
+  const [audioMass, setAudioMass] = useState([2, 3, 4, 5, 6, 7]);
+  const [startAudio, setStartAudio] = useState(false);
+  console.log(audioMass);
   const onClickOrder = (index) => {
     dispatch(removeOrder(index));
   };
 
   const onChangeInput = (event) => {
-    setNumber(Number(event.target.value));
+    setGetNumber(Number(event.target.value));
   };
-
-  // Воспроизводит любое аудио
-  function playAudio(fileName) {
-    // Промис использую для того, чтобы через "await" ждать завершения аудио
-    // см. внизу в функцию "onclickSetOrder"
-    return new Promise((resolve) => {
-      // Создаем аудио по названию файла fileName
-      const audio = new Audio(`/mp3/${fileName}.mp3`);
-      // Сразу воспроизводим аудио
-      audio.play();
-
-      // Ставим обработчик события на завершение воспроизведения аудио.
-      // Когда аудио завершится, то вызовется функция resolve
-      // Проще говоря, это завершение выполнения промиса
-      audio.addEventListener('ended', resolve);
-    });
-  }
-
-  // Воспроизводим одно целое число или несколько чисел
-  async function playNumbers(value) {
-    // Получаемстаток от десятичного числа
-    const restNumb = value % 10; // 46 % 10 = 6
-
-    // Получаем десятичное число
-    const bigNumb = value - restNumb; // 46 - 6 = 40
-
-    // Теперь у нас есть "40" и "6" по отдельности.
-
-    // Если число от 11 до 19, то воспроизводим отдельные аудио
-    if (value >= 11 && value <= 19) {
-      await playAudio(value);
-      return;
-    }
-
-    // Иначе воспроизводим по кускам данное число
-    await playAudio(bigNumb);
-    await playAudio(restNumb);
-
-    // Или можно сделать воспроизведение чуть быстрей
-    // Я сделал ниже код без "await", чтобы чуть быстрей воспроизводил номер заказа (тут уже на усмотрение)
-    // Ты можешь проверить разницу, закоментировав верхнее воспроизведение и оставив ниже код
-    // playAudio(bigNumb);
-    // setTimeout(() => playAudio(restNumb), 800);
-  }
-
-  const addOrderAndCleanForm = () => {
-    dispatch(setOrder(number));
-    setNumber('');
-    inputRef.current.focus();
-  };
-
   const onClickAddOrder = async () => {
-    if (!number) {
-      return alert('Укажите номер заказа!');
+    if (!getNumber) {
+      return alert("Укажите номер заказа!");
     }
 
-    if (number > 50) {
-      return alert('Слишком большой номер заказа!');
+    if (getNumber > 50) {
+      return alert("Слишком большой номер заказа!");
     }
 
     // Добавляем заказ в список и очищаем поле
     addOrderAndCleanForm();
+    appendAudio(getNumber);
 
     // Воспроизводим "Заказ номер" и ждём его завершения
-    await playAudio('main');
+    // await playAudio("main");
 
     // Потом воспроизводим номер заказа
-    await playNumbers(number);
+    // await playNumbers(number);
+  };
+
+  const addOrderAndCleanForm = () => {
+    dispatch(setOrder(getNumber));
+    setGetNumber("");
+    inputRef.current.focus();
+  };
+
+  function appendAudio(number) {
+    setAudioMass([...audioMass, number]);
+    console.log(audioMass);
+    test(audioMass[0]);
+  }
+
+  const test = (number) => {
+    if (startAudio) return false;
+    const audio = new Audio(`/mp3/main.mp3`);
+    audio.play();
+    setStartAudio(true);
+    audio.addEventListener("ended", function () {
+      if (number >= 1 && number <= 19) {
+        const audioNumber = new Audio(`/mp3/${number}.mp3`);
+        audioNumber.play();
+        audioNumber.addEventListener("ended", function () {
+          console.log("Срабатывает позднее");
+        });
+      } else {
+        const bigNumber = new Audio(`/mp3/${number - (number % 10)}.mp3`);
+        bigNumber.play();
+        bigNumber.addEventListener("ended", function () {
+          const minNumber = new Audio(`/mp3/${number % 10}.mp3`);
+          minNumber.play();
+          minNumber.addEventListener("ended", function () {});
+        });
+      }
+      setStartAudio(false);
+      setAudioMass(audioMass.shift());
+      if (audioMass.length > 0) {
+        // console.log(audioMass);
+
+        test(audioMass[0]);
+      }
+    });
+
+    console.log("Срабатывает первее");
+
+    // return new Promise((resolve) => {
+    //   const audio = new Audio(`/mp3/main.mp3`);
+    //   audio.play();
+    //   audio.addEventListener("ended", function () {
+    //     if (number >= 1 && number <= 19) {
+    //       const audioNumber = new Audio(`/mp3/${number}.mp3`);
+    //       audioNumber.play();
+    //       console.log(1);
+    //       audioNumber.addEventListener("ended", resolve);
+    //     } else {
+    //       const bigNumber = new Audio(`/mp3/${number - (number % 10)}.mp3`);
+    //       bigNumber.play();
+    //       bigNumber.addEventListener("ended", function () {
+    //         const minNumber = new Audio(`/mp3/${number % 10}.mp3`);
+    //         minNumber.play();
+    //         console.log(1);
+    //         minNumber.addEventListener("ended", resolve);
+    //       });
+    //     }
+    //   });
+    //   console.log(2);
+    // });
   };
 
   return (
@@ -100,8 +121,8 @@ function NewOrder() {
               <input
                 ref={inputRef}
                 onChange={(event) => onChangeInput(event)}
-                value={number}
-                type={'number'}
+                value={getNumber}
+                type={"number"}
                 min="1"
                 max="50"
               />
